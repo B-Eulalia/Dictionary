740082afb975bd85fc29f9cde0133ab2a5626c1c
B-Eulalia <101184278+B-Eulalia@users.noreply.github.com>
2022-04-10 14:44:44 +0300

    Create dictionary.cs

---
+++ dictionary.cs
@@ -0,0 +1,22 @@
+body {
+background-color: #fefbd8;
+}
+
+h1 {
+text-align: center;
+color: green;
+}
+
+input {
+color: saddlebrown;
+background-color: lightgrey;
+}
+
+label {
+color:  firebrick;
+}
+
+ul.a {
+list-style-type: circle;
+}
+        
